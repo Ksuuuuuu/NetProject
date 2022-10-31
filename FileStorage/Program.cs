0582ba2edb25be09ce6dc1dfc61619520f2f451b
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2022-10-31 11:34:40 +0300

    AddDbContext

--- FileStorage/Program.cs
+++ FileStorage/Program.cs
@@ -1,17 +1,23 @@
 using FileStorage.AppConfiguration.ServicesExtensions;
 using FileStorage.AppConfiguration.ApplicationExtensions;
+using FileStorage.Entities;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+
+var configuration = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json", optional: false)
+.Build();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.AddSerilogConfiguration();
+builder.Services.AddDbContextConfiguration(configuration);
 builder.Services.AddVersioningConfiguration();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerConfiguration();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 
 var app = builder.Build();
 
@@ -24,9 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
